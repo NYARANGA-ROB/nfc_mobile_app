b3defa9f12145b242e70e8226149e3719ff47215
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-09-04 18:44:41 +0300

    Format generated_plugin_registrant.dart file

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,7 +5,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:url_launcher_web/url_launcher_web.dart';
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
