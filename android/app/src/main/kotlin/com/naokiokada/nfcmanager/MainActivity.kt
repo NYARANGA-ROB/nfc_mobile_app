0c6498a8c6b698151406686746900aa41eaa1d32
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-09-09 14:20:19 +0300

    Refactor MainActivity.kt for clarity and formatting

--- android/app/src/main/kotlin/com/naokiokada/nfcmanager/MainActivity.kt
+++ android/app/src/main/kotlin/com/naokiokada/nfcmanager/MainActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.nfc.NfcAdapter
 import io.flutter.embedding.android.FlutterActivity
 
-/// Workaround for disabling the default-tag-detection while the app is foreground.
+/// Workaround for disabling the default-tag-detection while the app is foreg
 class MainActivity: FlutterActivity() {
   override fun onResume() {
     super.onResume()
